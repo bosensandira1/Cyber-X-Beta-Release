bbf30afa0f4d8090170e77564b8b8c83733f5c4e
Bosen <113291456+bosensandira1@users.noreply.github.com>
2022-09-22 00:01:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -43,24 +43,24 @@ global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['94701537259']
+global.ownertag = '94701537259' //ur tag number
+global.botnumber = '94701537259' //Add your Bot Number For Log
+global.botname = 'Lizard-X BETA EDITION' //ur bot name
+global.ig = "https://www.youtube.com/channel/UCENy44aOTDhirOnzRVJHzxQ" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.youtube.com/channel/UCENy44aOTDhirOnzRVJHzxQ" //ur website to be displayed
+global.sc = 'https://github.com/bosensandira1' //script link
+global.linkz = 'https://www.youtube.com/channel/UCENy44aOTDhirOnzRVJHzxQ'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
+global.packname = "Lizard-X" //ur sticker watermark packname
+global.author = "Lizard-X" //ur sticker watermark author
+global.footer = "® Lizard-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.aliveimg ="https://images.hdqwalls.com/wallpapers/neon-bot-ck.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.welcomeimg= "https://wallpapercave.com/dwp1x/wp9283531.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.alivemsg = ""
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94701537259'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
